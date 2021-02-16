cb35f6bebb9ab11b615bba92e0a5c53320aded18
RamishFatimaa <64559286+RamishFatimaa@users.noreply.github.com>
2021-02-16 20:25:43 +0500

    Update post-docker.sh

--- post-docker.sh
+++ post-docker.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/bash
 
-echo "Removing the previous reports.zip file..."
-
-rm reports.zip
-
-echo "Zipping the reports folder..."
-
-zip -r reports.zip reports
-
 echo "Removing the html folder and the logs file..."
 
-rm -r reports/html
-rm reports/log
+rm -r reports/output/html
+rm reports/output/log
 
 echo "Removing the docker container..."
 
